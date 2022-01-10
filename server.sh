b9d3db8a2e8176626a024aab91574d053e5e9abd
Pauldonald2 <77541819+Pauldonald2@users.noreply.github.com>
2022-01-10 13:49:52 +0100

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,15 @@
+sudo mkdir server &&
+cd server &&
+wget https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar && 
+sudo apt-get install default-jdk &&
+sudo apt-get install unzip &&
+wget http://survivalmcnew.ddns.net/start.zip &&
+unzip start.zip &&
+wget http://survivalmcnew.ddns.net/eula.zip &&
+unzip eula.zip
+java -Xmx1024M -Xms1024M -jar spigot-1.16.5.jar nogui
+
+
+
+
+
